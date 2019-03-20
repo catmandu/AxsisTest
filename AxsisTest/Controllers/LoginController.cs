fda0f2e3a7429e4ce1be94f049562542c1bbb052
Juan Carlos Uriarte Manzano <juanuri-c@herbalife.com>
2019-03-20 17:38:51 -0600

    Init

---
+++ AxsisTest/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Mvc;
+using AxsisTest.Models;
+
+namespace AxsisTest.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Usuario model)
+        {
+            if (ModelState.IsValid)
+            {
+                if(model.NombreUsuario.Equals("usuario")
+                    && model.Password.Equals("mipass"))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
+    }
+}
